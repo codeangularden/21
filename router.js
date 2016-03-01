91386bdc22b97a1c4565b5230d0115b81010b663
codeangularden <ganeshkraskar@rediffmail.com>
2016-03-01 11:22:58 +0530

    Added files via upload

---
+++ router.js
@@ -0,0 +1,21 @@
+﻿app.config(function ($routeProvider) {
+    $routeProvider
+     .when('/Add', {
+         templateUrl: '/Views/Add.html',
+         controller: 'studCtrl'
+
+     })
+    $routeProvider
+     .when('/List', {
+         templateUrl: '/Views/List.html',
+         controller: 'listCtrl'
+     })
+
+     .when('/Add/:sid', {
+         templateUrl: '/Views/Add.html',
+         controller: 'studCtrl'
+     })
+     .otherwise({
+         redirectTo: '/Add'
+     });
+});
