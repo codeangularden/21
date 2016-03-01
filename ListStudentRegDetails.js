5f4ca78005cb44b9f5ec5bb8be002b8ec051a4d5
codeangularden <ganeshkraskar@rediffmail.com>
2016-03-01 10:58:40 +0530

    Added files via upload

---
+++ ListStudentRegDetails.js
@@ -0,0 +1,37 @@
+﻿
+app.controller('listCtrl', function ($scope, studentregFactory, $rootScope) {
+
+    //Find All Student Details
+    $scope.getStudentData = function () {
+        studentregFactory.getAllStudnets().success(function (data) {
+            $scope.studentinDb = JSON.parse(data.d);
+            //$scope.sortOrder = false;
+            $scope.success = true;
+        })
+    };
+
+    $scope.fnDelete = function (id) {
+        studentregFactory.deleteStudent(id).success(function (data) {
+
+            if (data.d == true) {
+                $scope.showMessage = true;
+                $scope.message = "Student data Deleted Successfully";
+                $scope.getStudentData();
+            }
+            else {
+                $scope.showMessage = true;
+                $scope.message = "Student data not deleted Successfully";
+            }
+        })
+    };
+
+    $scope.sortOrder= false;
+    $scope.orderByMe = function (x) {
+        $scope.myOrderBy = x;
+        $scope.sortOrder = !$scope.sortOrder;
+    };
+
+
+    $scope.getStudentData();
+
+});
---
+++ studentRegFactory.js
@@ -0,0 +1,53 @@
+﻿app.factory('studentregFactory', function ($http) {
+
+    var service = {}
+
+    service.saveStudent = function (stud) {
+
+        return $http({
+            url: "StudentRegistration.aspx/saveStudent",
+            method: "POST",
+            data: { StudentObj: angular.toJson(stud) }
+        })
+    };
+
+
+    service.getAllStudnets = function () {
+        return $http({
+            url: "StudentRegistration.aspx/getAllStudent",
+            method: "GET",
+            data: {},
+            headers: { "Content-Type": "application/json" }
+        })
+    };
+
+    service.deleteStudent = function (id) {
+        return $http({
+            url: "StudentRegistration.aspx/delete",
+            method: "POST",
+            data: { sid: id }
+        })
+    };
+
+    service.editStudent = function (student) {
+        console.log('testfac'+student);
+        return $http({
+            url: "StudentRegistration.aspx/editStudent",
+            method: "POST",
+            data: { StudentObj: angular.toJson(student) }
+        })
+    };
+
+    service.findStudentByID = function (sid) {
+        return $http({
+            url: "StudentRegistration.aspx/findStudentByID",
+            method: "POST",
+            data: { sid: sid }
+        })
+    };
+
+
+
+    return service;
+
+});
